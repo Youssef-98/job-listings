14a52d1ec7e68529d76884f226a093a65c81c2de
Youssef El Hindaz <youssef.elhindaz@student.ap.be>
2021-05-20 00:17:56 +0200

    initialize project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-blue-600 text-green-200">Job Listings</h1>
+    </div>
+  );
+}
+
+export default App;
