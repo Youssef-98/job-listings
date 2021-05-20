ec209bfbf2e56e9854274c4cec7d273f600f64fb
Youssef El Hindaz <youssef.elhindaz@student.ap.be>
2021-05-20 20:18:54 +0200

    category bug fixed

--- src/components/JobCategories.js
+++ src/components/JobCategories.js
@@ -3,12 +3,12 @@ import React from 'react';
 // Dynamic component exclusive to languages and tools since they're arrays
 const JobCategories = ({ items }) => {
   return (
-    <div className="category">
+    <>
       {
         items.map(item => {
           return (
             <div 
-              className="inline-block gap-x-6"
+              className="category"
               key={item}
             >
               {item}
@@ -16,7 +16,7 @@ const JobCategories = ({ items }) => {
           )
         })
       }
-    </div>
+    </>
   )
 }
 
